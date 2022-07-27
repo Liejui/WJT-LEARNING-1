1431d96780d96f843cc5d5d00d917e808e646fd9
Your Name <you@example.com>
2022-07-27 10:41:35 +0800

    添加学生类

---
+++ information/src/Study.java
@@ -0,0 +1,18 @@
+public class Study {
+
+    public String studentNo;
+    public String studentName;
+    public int studentAge;
+    public String studentMale;
+    public String studentSchool;
+    public int getStudentScore;
+    public void GoToNightClass(){
+        System.out.println("我们在认真上晚自习");
+    }
+    public void GoToLibrary(){
+        System.out.println("我们在图书馆");
+    }
+    public void DoExercise(){
+        System.out.println("我们正在做操");
+    }
+}
